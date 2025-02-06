0b0439c9c49f1a2a076bd0ac4d2338c54c452329
ickyDev <67420833+ickydev@users.noreply.github.com>
2025-02-06 19:05:18 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,188 @@
+let borda;
+let bordaLargura = 360;
+let bordaAltura = 640;
+let context;
+
+// Passaro
+let passaroLargura = 34;
+let passaroAltura = 24;
+let passaroX = bordaLargura / 8;
+let passaroY = bordaAltura / 2;
+let birding;
+
+// Flappy
+let passaro ={
+    x: passaroX,
+    y: passaroY,
+    largura: passaroLargura,
+    altura: passaroAltura
+}
+
+// Canos
+let canosArray = [];
+let canoLargura = 64;
+let canoAltura = 512;
+let canoX = bordaLargura;
+let canoY = 0;
+
+let dificuldade = 2;
+
+// Img
+let canoCimaImg;
+let canoBaixoImg;
+
+// Física
+let velocidadeX = -2 * dificuldade;
+let velocidadeY = 0;
+let gravidade = 0.4;
+
+let gameOver = false;
+let pontos = 0;
+
+window.onload = () => {
+    borda = document.getElementById("borda");
+    borda.width = bordaLargura;
+    borda.height = bordaAltura;
+    context = borda.getContext("2d");
+
+    //
+    birding = new Image();
+    birding.src = "/src/images/flappybird.png";
+    birding.onload = () => {
+        context.drawImage(birding, passaro.x, passaro.y, passaro.largura, passaro.altura);
+    }
+
+    canoCimaImg = new Image();
+    canoCimaImg.src = "src/images/toppipe.png";
+
+    canoBaixoImg = new Image();
+    canoBaixoImg.src = "src/images/bottompipe.png";
+
+    requestAnimationFrame(Update)
+    setInterval(AdicionarCanos , 1500 / dificuldade);
+    document.addEventListener("keydown", MoverPassaro);
+}
+
+function Update(){
+    requestAnimationFrame(Update);
+
+    if(gameOver){
+        return;
+    }
+
+    context.clearRect(0,0, borda.width, borda.height);
+
+    // Flappy
+    velocidadeY += gravidade;
+    passaro.y += velocidadeY;
+    passaro.y = Math.min(passaro.y, borda.height - passaro.altura);
+    passaro.y = Math.max(passaro.y, 0);
+    context.drawImage(birding, passaro.x, passaro.y, passaro.largura, passaro.altura);
+
+    if(passaro.y == 0 || passaro.y == borda.height - passaro.altura){
+        gameOver = true;
+    }
+
+    // Canos
+    for(let i = 0; i < canosArray.length; i++){
+        let cano = canosArray[i];
+        cano.x += velocidadeX;
+        context.drawImage(cano.img, cano.x, cano.y, cano.largura, cano.altura);
+
+        if(!cano.passou && passaro.x > cano.x + cano.largura){
+            cano.passou = true;
+            pontos += 0.5;
+        }
+        if(DetectarColisao(passaro, cano)){
+            gameOver = true;
+        }
+    }
+
+    while(canosArray.length > 0 && canosArray[0].x < -canoLargura){
+        canosArray.shift();
+    }
+
+    context.fillStyle = "white";
+    context.font = "4rem pixelify sans";
+    context.fillText(pontos, bordaLargura / 2, 80);
+
+    if(gameOver){
+        context.fillStyle = "white";
+        context.fillStyle = "text-type: bold";
+        context.font = "2.6rem pixelify sans";
+        context.fillText("Fim de Jogo", bordaLargura/5, bordaAltura/2);
+        context.font = "1.2rem pixelify sans";
+        context.fillText("Aperte espaço para recomeçar", bordaLargura/10, bordaAltura/2 + 30);
+    }
+
+    switch(pontos){
+        case 0:
+            dificuldade = 1;
+            break;
+        case 5:
+            dificuldade = 1.25;
+            break;
+        case 10:
+            dificuldade = 1.5;
+            break;
+        case 10:
+            dificuldade = 1.75;
+            break;
+        case 15:
+            dificuldade = 2;
+            break;
+        case 20:
+            dificuldade = 2.5;
+            break;
+    }
+}
+
+function AdicionarCanos(){
+    if(gameOver){
+        return;
+    }
+    let canoRandY = canoY - canoAltura / 4 - Math.random() * (canoAltura / 2);
+    let abertura = canoAltura / (2 * dificuldade);
+
+    let canoCima = {
+        img: canoCimaImg,
+        x: canoX,
+        y: canoRandY,
+        largura: canoLargura,
+        altura: canoAltura,
+        passou: false
+    }
+
+    canosArray.push(canoCima);
+
+    let canoBaixo = {
+        img: canoBaixoImg,
+        x: canoX,
+        y: canoRandY + canoAltura + abertura,
+        largura: canoLargura,
+        altura: canoAltura,
+        passou: false
+    }
+
+    canosArray.push(canoBaixo);
+}
+
+function MoverPassaro(e){
+    if(e.code == "ArrowUp" || e.code == "Space" || e.code == "KeyX"){
+        velocidadeY = -6;
+    }
+
+    if(gameOver){
+        passaro.y = borda.height / 2 - passaro.altura / 2;
+        pontos = 0;
+        canosArray = [];
+        gameOver = false;
+    }
+}
+
+function DetectarColisao(a,b){
+    return a.x < b.x + b.largura &&
+    a.x + a.largura > b.x &&
+    a.y < b.y + b.altura &&
+    a.y + a.altura > b.y
+}
